c72f86b62179aeeceeaf441498fa5ec16975e0bb
munteanu94 <117288866+munteanu94@users.noreply.github.com>
2024-02-22 21:02:20 +0200

    I finished page ShowNow and I add buttons filter  an change some photes of good quality

--- src/componente/pages/AllProducts/AllProduscts.jsx
+++ src/componente/pages/AllProducts/AllProduscts.jsx
@@ -1,36 +1,103 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import Axios from "axios";
+import { Link } from "react-router-dom";
 
-export default function AllProducts() {
-  const [data, setData] = useState(0);
+import { FaRegHeart } from "react-icons/fa";
+import { IoEyeOutline } from "react-icons/io5";
+import { ShopContext } from "../homeComponente/sectionSecond/subComponete/Context";
+import Star from "../homeComponente/sectionSecond/subComponete/StarsFeedback";
+import "./AllProduscts.css";
+export default function AllProducts(props) {
+  const [data, setData] = useState([]);
+  const [category, setCategory] = useState(data);
 
+  const { addToCart, cartItems } = useContext(ShopContext);
   useEffect(() => {
-    const fetchData = async () => {
-      const url = "https://imdb146.p.rapidapi.com/v1/find/?query=love";
-      const options = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key":
-            "f570871e4emsh4b47b548b95dfeep199f97jsn61147652d32b",
-          "X-RapidAPI-Host": "imdb146.p.rapidapi.com",
-        },
-      };
-      try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
+    Axios("https://fakestoreapi.com/products/").then((res) => {
+      console.log(res.data);
+      setData(res.data);
+      setCategory(res.data);
+    });
   }, []);
-
+  const filterItems = (catItem) => {
+    const updateItems = data.filter((item) => {
+      return item.category === catItem;
+    });
+    setCategory(updateItems);
+  };
   return (
     <div>
-      <h1>Hello page</h1>
-      <div></div>
+      <p
+        style={{
+          marginTop: 50,
+          marginLeft: 80,
+        }}
+      >
+        <span
+          style={{
+            color: "silver",
+          }}
+        >
+          Home
+        </span>
+        / Show Now
+      </p>
+      <div className="btnsShowNow">
+        <button onClick={() => setCategory(data)}>All</button>
+        <button onClick={() => filterItems("men's clothing")}>
+          Men's clothing
+        </button>
+        <button onClick={() => filterItems("women's clothing")}>
+          Women's Clothing
+        </button>
+        <button onClick={() => filterItems("jewelery")}>Jewelery</button>
+        <button onClick={() => filterItems("electronics")}>Electronics</button>
+      </div>
+      <div className="containerShowNow">
+        {category.map((user) => (
+          <div className="sliderCart cartShowNow">
+            <div className="containerCart">
+              <div className="containerImg">
+                <Link to="/productDetails">
+                  <img src={user.image} alt="cart" />
+
+                  <div className="icon heart">
+                    <FaRegHeart />
+                  </div>
+                  <div className="icon eyes">
+                    <IoEyeOutline />
+                  </div>
+                </Link>
+                <div className="ContainerAdd">
+                  <button className="btnAdd" onClick={() => addToCart(user.id)}>
+                    Add To Cart
+                    {cartItems[user.id] > 0 && <>({cartItems[user.id]})</>}
+                  </button>
+                </div>
+              </div>
+
+              <div>
+                <div className="textCart">
+                  <h5>{user.title}</h5>
+
+                  <div className="cartprice">
+                    <span>${user.price}</span>
+                  </div>
+                  <div className="feedback">
+                    <div>
+                      <Star
+                        stars={user.rating.rate}
+                        reviews={user.rating.count}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
