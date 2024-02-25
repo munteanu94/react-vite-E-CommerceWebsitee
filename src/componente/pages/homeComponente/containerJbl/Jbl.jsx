401560bb322fcad79b620874fb34da22ffd4202f
munteanu94 <117288866+munteanu94@users.noreply.github.com>
2024-02-25 23:04:55 +0200

    I stat to make time sales

--- src/componente/pages/homeComponente/containerJbl/Jbl.jsx
+++ src/componente/pages/homeComponente/containerJbl/Jbl.jsx
@@ -1,7 +1,38 @@
 import { Link } from "react-router-dom";
 import imageJbl from "../containerJbl/jbl.png";
+import React, { useState, useEffect } from "react";
 import "./Jbl.css";
 export default function BoxJbl() {
+  const [timeLeft5d, setTimeLeft5d] = useState(5 * 24 * 60 * 60);
+  const [timeLeft, setTimeLeft] = useState(24 * 60 * 60);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (timeLeft5d > 0) {
+        setTimeLeft5d((prevTime) => prevTime - 1);
+      } else if (timeLeft > 0) {
+        setTimeLeft((prevTime) => prevTime - 1);
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+  useEffect(() => {
+    const resetTime = () => {
+      setTimeLeft(24 * 60 * 60);
+    };
+    window.addEventListener("focus", resetTime);
+    return () => window.removeEventListener("focus", resetTime);
+  }, []);
+
+  const formatTime = (time) => {
+    const days = Math.floor(time / 24);
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+    return `${hours.toString().padStart(2, "0")} : ${minutes
+      .toString()
+      .padStart(2, "0")} : ${seconds.toString().padStart(2, "0")}`;
+  };
+
   return (
     <div className="containerJbl">
       <div className="text">
